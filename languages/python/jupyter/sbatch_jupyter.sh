6d48a3b04d9e9a6bfc32708cc311e447f343c48f
diskwarrior <48100115+diskwarrior@users.noreply.github.com>
2021-03-29 14:50:14 -0600

    remove account and partition directives

--- languages/python/jupyter/sbatch_jupyter.sh
+++ languages/python/jupyter/sbatch_jupyter.sh
@@ -2,10 +2,8 @@
 
 #SBATCH --time=01:00:00
 #SBATCH --nodes=1
-#SBATCH --partition=short
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=36
-#SBATCH --account=<account-handle>
 
 module purge
 module load conda
