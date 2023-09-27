79cc6693462693dbc063e8ce459951e7523c37be
Kinshuk <kinshuk.panda@nrel.gov>
2023-09-27 13:55:16 -0600

    reverted back changes

--- applications/plexos/RunFiles/submit_simple.sh
+++ applications/plexos/RunFiles/submit_simple.sh
@@ -5,8 +5,6 @@
 #SBATCH --partition=debug
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUE
 #SBATCH --job-name="PLSimple"
-#SBATCH --out=%J.out
-#SBATCH --error=%J.err
 
 module purge
 module load craype-x86-spr
@@ -14,4 +12,4 @@ module load gurobi/10.0.2 plexos/9.200R06
 
 cd /scratch/${USER}/HPC/applications/plexos/RunFiles/
 
-$PLEXOS/PLEXOS64 -n 5_bus_system_v2.xml -m 2024_yr_15percPV_Gurobi -cu nrelplexos -cp Nr3lplex0s
+$PLEXOS/PLEXOS64 -n 5_bus_system_v2.xml -m 2024_yr_15percPV_Gurobi -cu nrelplexos -cp Nr3lplex0s > fout_${SLURM_JOB_ID} 2>&1
