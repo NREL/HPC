3b7f62da73f4b0cc9dc7c9a11fe73b8aeebd586e
Olivia Hull <39199786+oahull0112@users.noreply.github.com>
2024-04-22 14:22:36 -0500

    Update load_PrgEnv_intel.sh

--- kestrel/mpi_version_check/load_PrgEnv_intel.sh
+++ kestrel/mpi_version_check/load_PrgEnv_intel.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 # clear the module environment
-module purge
-
-# remove any NREL-built modules from module path
-module unuse /nopt/nrel/apps/modules/default/compilers_mpi/
-module unuse /nopt/nrel/apps/modules/default/utilities_libraries/
-module unuse /nopt/nrel/apps/modules/default/applications/
-
-# load the Intel programming environment (Intel oneapi compilers with Cray MPICH)
-module load craype-x86-spr
-module load PrgEnv-intel
+module restore
+module swap PrgEnv-cray PrgEnv-intel
+module unload cray-libsci
