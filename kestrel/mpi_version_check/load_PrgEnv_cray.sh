f3e38104b9485b21e7fe3f4154b33f09f4c37840
Olivia Hull <39199786+oahull0112@users.noreply.github.com>
2024-04-22 14:23:28 -0500

    Update load_PrgEnv_cray.sh

--- kestrel/mpi_version_check/load_PrgEnv_cray.sh
+++ kestrel/mpi_version_check/load_PrgEnv_cray.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# clear the module environment
-module purge
-
-# remove any NREL-built modules from module path
-module unuse /nopt/nrel/apps/modules/default/compilers_mpi/
-module unuse /nopt/nrel/apps/modules/default/utilities_libraries/
-module unuse /nopt/nrel/apps/modules/default/applications/
-
-# load the Cray programming environment (Cray CCE compilers with Cray MPICH)
-module load craype-x86-spr
-module load PrgEnv-cray
+# restores default environment, which is PrgEnv-cray
+# we want to use module restore rather than module purge; module load PrgEnv-cray
+# to preserve some environment variables.
+module restore
